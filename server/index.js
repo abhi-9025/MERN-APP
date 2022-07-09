6b35a7e621c7f08dfeeeda23264c79e30b560f71
Abhi <abhisaroj192001@gmail.com>
2022-07-09 22:24:57 +0530

    added Post and get Api's

--- server/index.js
+++ server/index.js
@@ -1,13 +1,63 @@
-const express=require('express')
-const app=express()
-const cors=require('cors')
-const pool = require("./db")
+const express = require("express");
+const app=express();
+const cors=require("cors");
+const pool =require("./db");
 
 //middleware
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json)
+
+
+// Routes
+
+// create a todo
+
+
+app.post("/todos",async(req,res)=>{
+    try {
+         const {description} = req.body;
+         const newtodo=await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING * ",[description]
+         );
+
+         res.json(newtodo.rows[0]);
+
+    } catch (err) {
+      console.error(err.message)
+    }
+});
+
+//get all todo
+app.get("/todos",async(req,res)=>{
+    try {
+        const alltodos=await pool.query("SELECT * FROM todo");
+        res.json(alltodos.rows)
+        
+    } catch (err) {
+        console.error(err.message+"jdbfhgcb")
+        
+    }
+})
+// get a todo
+
+app.get("/todos/:id",async(res,req)=>{
+
+    try {
+        const { id } =req.params;
+        const todo=await pool.query("SELECT * FROM todo where todo_id=$1",[id]);
+        res.json(todo.rows[0]);
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
+//update a todo
+
+// delete a todo
 
 
 

348d2d50d3043acd2313e91f1a7ab4b2489b4000
Abhi <abhisaroj192001@gmail.com>
2022-07-09 12:59:14 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express=require('express')
 const app=express()
 const cors=require('cors')
+const pool = require("./db")
 
 //middleware
 
 app.use(cors())
+
+app.use(express.json)
+
+
 
 app.listen(5000,()=>{
     console.log("server started listenig on port no 5000")
